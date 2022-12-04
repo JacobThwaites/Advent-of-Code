08874c36822f7435974ca1883b670d778a35840b
Jake Thwaites <jakethwaites@hotmail.co.uk>
2022-12-04 12:30:21 +0100

    add 2022 day 4 solution

--- 2022/day_4/solution.py
+++ 2022/day_4/solution.py
@@ -18,9 +18,47 @@ def get_input():
         input = []
         for line in file: 
             line = line.replace('\n', '')
-            input.append(line)
+            a,b = line.split(',')
+            ax = a.split('-')
+            bx= b.split('-')
+            for i, v in enumerate(ax):
+                ax[i] = int(ax[i])
+                bx[i] = int(bx[i])
+            
+            c = [ax, bx]
+            input.append(c)
 
         return input 
 
 input = get_input()
-print(input)
+# print(input)
+
+# Part 1
+# p = 0
+
+# for x in input:
+#     a = x[0]
+#     b = x[1]
+
+#     if a[0] >= b[0] and a[1] <= b[1]:
+#         p += 1
+#     elif b[0] >= a[0] and b[1] <= a[1]:
+#         p+=1
+
+# print(p)
+
+
+# Part 2
+
+p = 0
+
+for x in input:
+    a = x[0]
+    b = x[1]
+
+    if (a[0] >= b[0] and a[0] <= b[1]) or (a[1] >= b[0] and a[1] <= b[1]):
+        p += 1
+    elif (b[0] >= a[0] and b[0] <= a[1]) or (b[1] >= a[0] and b[1] <= a[1]):
+        p += 1
+
+print(p)
