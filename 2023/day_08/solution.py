5c981160757cfa788b983d28c67db78fce38d84c
Jacob Thwaites <jakethwaites@gmail.com>
2023-12-09 16:04:46 +0100

    2023 day 8 part 1 complete

--- 2023/day_08/solution.py
+++ 2023/day_08/solution.py
@@ -20,9 +20,66 @@ def get_input():
         input = []
         for line in file: 
             line = line.replace('\n', '')
+            line = line.split(' = ')
             input.append(line)
 
         return input 
 
 input = get_input()
-print(input)
+
+instructions = ''
+
+nodes = []
+
+network = {}
+
+for i, line in enumerate(input): 
+    if i == 0:
+        instructions = line[0]
+    elif line == ['']:
+        continue
+    else:
+        line[1] = line[1].replace('(','')
+        line[1] = line[1].replace(')','')
+        location = line[0]
+        l, r = line[1].split(', ')
+        network[line[0]] = (l, r)
+
+start = network['AAA']
+
+is_end = False
+steps = 0
+
+while not is_end:
+    for direction in instructions:
+        steps += 1
+        if direction == 'L':
+            if start[0] == 'ZZZ':
+                is_end = True 
+                break
+            start = network[start[0]]
+        else:
+            if start[1] == 'ZZZ':
+                is_end = True 
+                break
+            start = network[start[1]]
+
+# print(steps)
+
+
+# Part 2
+
+nodes_ending_with_a = []
+for n in network.keys():
+    if n[-1] == 'A':
+        nodes_ending_with_a.append(n)
+
+starts = nodes_ending_with_a
+
+while True:
+    distances_to_next_z = []
+    steps = 0
+
+    while len(distances_to_next_z) > 6:
+        for instruction in instructions:
+            pass
