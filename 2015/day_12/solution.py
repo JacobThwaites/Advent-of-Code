bad7a8de0a5878cfcf7beb792066ec19a69633a2
Jacob Thwaites <jakethwaites@gmail.com>
2023-12-15 21:12:47 +0100

    2015 day 12 complete

--- 2015/day_12/solution.py
+++ 2015/day_12/solution.py
@@ -3,6 +3,7 @@ import math
 import bisect
 from math import gcd,floor,sqrt,log
 from collections import defaultdict as dd
+import json
 from bisect import bisect_left as bl, bisect_right as br
 
 sys.setrecursionlimit(100000000)
@@ -18,77 +19,57 @@ def get_input():
     with open(filename, 'r') as file: 
         input = []
         for line in file: 
-            return line
+            return json.loads(line)
 
         return input 
 
 input = get_input()
+total = 0
 
-def increment(str):
-    new_last_char = chr(ord(str[-1]) + 1)
-    if new_last_char == '{':
-        if len(str) == 1:
-            return 'aa'
-        return increment(str[:-1]) + 'a'
-
-    return str[:-1] + new_last_char
-
-def has_three_consecutive(str):
-    for i, _ in enumerate(str[:-2]):
-        if ord(str[i]) == ord(str[i+1]) - 1 and ord(str[i+1]) == ord(str[i+2]) - 1:
-            return True
-        
-    return False
-
-def has_invalid_chars(str):
-    for char in str:
-        if char in 'iol':
-            print(char)
-            return True
+def count_nums(obj):
+    global total
+    if isinstance(obj, str):
+        return 
     
-    return False
+    if isinstance(obj, int):
+        total += obj 
+        print(total)
 
-def count_overlapping_pairs(str):
-    total = 0
+    if isinstance(obj, dict):
+        for v in obj.values():
+            count_nums(v)
 
-    i = 0
-    while i < len(str) - 1:
-        print(str[i], str[i+1])
-        if str[i] == str[i+1]:
-            total += 1
-            i+= 1
-        
-        i += 1
+    if isinstance(obj, list):
+        for entry in obj:
+            count_nums(entry)
+
+# count_nums(input)
+# print(total)
+
+# Part 2
+
+total = 0
+
+def count_nums(obj):
+    global total
+    if isinstance(obj, str):
+        return 
     
-    return total
-        
+    if isinstance(obj, int):
+        total += obj 
+        print(total)
 
-def is_valid(str):
-    three_consecutive = has_three_consecutive(str)
+    if isinstance(obj, dict):
+        for k, v in obj.items():
+            if k == 'red' or v == 'red':
+                return
+        for v in obj.values():
+            count_nums(v)
 
-    if not three_consecutive:
-        return False
-    
-    if has_invalid_chars(str):
-        return False
-    
-    overlapping_pairs = count_overlapping_pairs(str)
+    if isinstance(obj, list):
+        for entry in obj:
+            count_nums(entry)
 
-    if overlapping_pairs < 2:
-        return False
+count_nums(input)
 
-    return True
-
-password = input
-
-while not is_valid(password):
-    password = increment(password)
-
-print(password)
-
-password = increment(password)
-
-while not is_valid(password):
-    password = increment(password)
-
-print(password)
+print(total)
