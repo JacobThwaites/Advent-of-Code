e887dc4d76da2153fe46af5b65bf42001624b265
Jacob Thwaites <jakethwaites@gmail.com>
2023-12-15 21:12:35 +0100

    fix day name

--- 2015/day_11/solution.py
+++ 2015/day_11/solution.py
@@ -14,45 +14,81 @@ mod=1000000007
 
 
 def get_input():
-    with open('./input.txt', 'r') as file: 
+    filename = './input.txt'
+    with open(filename, 'r') as file: 
         input = []
         for line in file: 
-            line = line.replace('\n', '')
-            input.append(line)
+            return line
 
-        return input[0]
+        return input 
 
 input = get_input()
 
-def count(nums):
-    curr = nums[0]
-    total = 1
-    counts = []
-    for num in nums[1:]:
-        if num == curr:
+def increment(str):
+    new_last_char = chr(ord(str[-1]) + 1)
+    if new_last_char == '{':
+        if len(str) == 1:
+            return 'aa'
+        return increment(str[:-1]) + 'a'
+
+    return str[:-1] + new_last_char
+
+def has_three_consecutive(str):
+    for i, _ in enumerate(str[:-2]):
+        if ord(str[i]) == ord(str[i+1]) - 1 and ord(str[i+1]) == ord(str[i+2]) - 1:
+            return True
+        
+    return False
+
+def has_invalid_chars(str):
+    for char in str:
+        if char in 'iol':
+            print(char)
+            return True
+    
+    return False
+
+def count_overlapping_pairs(str):
+    total = 0
+
+    i = 0
+    while i < len(str) - 1:
+        print(str[i], str[i+1])
+        if str[i] == str[i+1]:
             total += 1
-        else:
-            counts.append((curr, total))
-            curr = num
-            total = 1
-    counts.append((curr, total))
+            i+= 1
+        
+        i += 1
+    
+    return total
+        
 
-    return counts
+def is_valid(str):
+    three_consecutive = has_three_consecutive(str)
 
-def say(nums):
-    s = ''
-    for num, count in nums:
-        s += str(count) + num
+    if not three_consecutive:
+        return False
+    
+    if has_invalid_chars(str):
+        return False
+    
+    overlapping_pairs = count_overlapping_pairs(str)
 
-    return s
+    if overlapping_pairs < 2:
+        return False
 
-def count_and_say(str):
-    c = count(str)
-    return say(c)
+    return True
 
-s = input
+password = input
 
-for _ in range(50):
-    s = count_and_say(s)
+while not is_valid(password):
+    password = increment(password)
 
-print(len(s))
+print(password)
+
+password = increment(password)
+
+while not is_valid(password):
+    password = increment(password)
+
+print(password)
