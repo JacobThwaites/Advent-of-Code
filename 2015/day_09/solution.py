20fdbc0d0ca4b29e45a199c9a79f3fd81c559ac1
Jacob Thwaites <jakethwaites@gmail.com>
2023-12-19 18:17:54 +0100

    rename folders

---
+++ 2015/day_09/solution.py
@@ -0,0 +1,33 @@
+import sys
+import math
+import bisect
+from math import gcd,floor,sqrt,log
+from collections import defaultdict as dd
+from bisect import bisect_left as bl, bisect_right as br
+
+sys.setrecursionlimit(100000000)
+
+ceil   =lambda x: int(x) if(x==int(x)) else int(x)+1
+ceildiv=lambda x,d: x//d if(x%d==0) else x//d+1
+
+mod=1000000007
+
+
+def get_input():
+    with open('./input.txt', 'r') as file: 
+        input = []
+        for line in file: 
+            line = line.replace('\n', '')
+            line = line.split(' ')
+            input.append((line[0], line[1], int(line[2])))
+
+        return input 
+
+input = get_input()
+print(input)
+
+graph = dd(dd)
+
+for start, end, distance in input:
+    graph[start][end] = distance
+
