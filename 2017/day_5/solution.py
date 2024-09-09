cb1be48b8251d4642634e932c4a591c888cd91c9
Jacob Thwaites <jakethwaites@gmail.com>
2024-09-09 22:54:57 +0200

    2017 day 5

---
+++ 2017/day_5/solution.py
@@ -0,0 +1,53 @@
+import sys
+import math
+import bisect
+from math import gcd,floor,sqrt,log
+from collections import defaultdict as dd
+from bisect import bisect_left as bl, bisect_right as br
+
+sys.setrecursionlimit(100000000)
+
+ceil   =lambda x: int(x) if(x==int(x)) else int(x)+1
+ceildiv=lambda x,d: x//d if(x%d==0) else x//d+1
+
+mod=1000000007
+
+
+def get_input():
+    filename = './input.txt'
+    # filename ='./test.txt'
+    with open(filename, 'r') as file: 
+        input = []
+        for line in file: 
+            line = line.replace('\n', '')
+            input.append(int(line))
+
+        return input 
+
+input = get_input()
+# [print(row) for row in input]
+
+# i = 0 
+# steps = 0
+# while i < len(input):
+#     temp = i
+#     i += input[i]
+#     input[temp] += 1
+#     steps += 1
+
+# print(steps)
+
+# Part 2
+
+i = 0
+steps = 0
+while i < len(input):
+    temp = i
+    i += input[i]
+    if input[temp] >= 3:
+        input[temp]-= 1
+    else:
+        input[temp] += 1
+    steps += 1
+
+print(steps)
