a41ce20105d1b4b94ee07f99d1f08aaef8b69ec3
Jacob Thwaites <jakethwaites@gmail.com>
2025-10-19 18:02:17 +0200

    2021 day 21 part 2

--- 2015/day_21/solution.py
+++ 2015/day_21/solution.py
@@ -1,192 +1,116 @@
 import sys
 import math
 import bisect
-from math import gcd,floor,sqrt,log
+from math import gcd, floor, sqrt, log
 from collections import defaultdict as dd
 from bisect import bisect_left as bl, bisect_right as br
 
 sys.setrecursionlimit(100000000)
 
-ceil   =lambda x: int(x) if(x==int(x)) else int(x)+1
-ceildiv=lambda x,d: x//d if(x%d==0) else x//d+1
 
-mod=1000000007
+def ceil(x): return int(x) if (x == int(x)) else int(x)+1
+def ceildiv(x, d): return x//d if (x % d == 0) else x//d+1
 
+
+mod = 1000000007
 
 def get_input():
     filename = './input.txt'
-    with open(filename, 'r') as file: 
-        input = []
-        type = ''
-        for i, line in enumerate(file): 
-            line = line.replace('\n', '')
-            line = line.split()
-            if not len(line):
+    with open(filename, 'r') as file:
+        items = []
+        type_ = ''
+        for line in file:
+            line = line.strip()
+            if not line:
                 continue
-            
-            if line[-1] == 'Armor':
-                type = line[0][:-1]
-                continue 
-            
-            item = {}
-            item['armor'] = int(line[-1])
-            item['damage'] = int(line[-2])
-            item['cost'] = int(line[-3])
-            item['type'] = type
-            input.append(item)
 
-        input = sorted(input, key=lambda x: x['cost'])
-        return input 
+            parts = line.split()
+            if parts[-1] == 'Armor' or parts[-1] == 'Weapons' or parts[-1] == 'Rings':
+                type_ = parts[0][:-1]
+                continue
 
-input = get_input()
-# for x in input:
-#     cx)
+            item = {
+                'type': type_,
+                'cost': int(parts[-3]),
+                'damage': int(parts[-2]),
+                'armor': int(parts[-1])
+            }
+            items.append(item)
 
-def get_ring_combinations():
-    ring_combinations = []
-    rings = []
-    for x in input:
-        if x['type'] == 'Rings':
-            rings.append(x)
+    return items
 
-    for i, x in enumerate(rings):
-        ring_combinations.append(x)
-        for j in range(i + 1, len(rings)):
-            y = rings[j]
-            combo = {}
-            combo['cost'] = y['cost'] + x['cost']
-            combo['armor'] = y['armor'] + x['armor']
-            combo['damage'] = y['damage'] + x['damage']
-            ring_combinations.append(combo)
-    return sorted(ring_combinations, key= lambda x: x['cost'])
 
-def get_armor_ring_combos():
-    ring_combos = get_ring_combinations()
-    armors = [x for x in input if x['type'] == 'Armor']
-    armor_ring_combos = []
+input_items = get_input()
+
+def get_armor_ring_combos(input_items):
+    armors = [x for x in input_items if x['type'] == 'Armor']
+    armors.append({'cost': 0, 'damage': 0, 'armor': 0,
+                  'type': 'Armor'})
+
+    rings = [x for x in input_items if x['type'] == 'Rings']
+    ring_combos = [[]]
+    ring_combos += [[r] for r in rings]
+    ring_combos += [list(c) for c in combinations(rings, 2)]
+
+    combos = []
     for a in armors:
-        armor_ring_combos.append(a)
-        for r in ring_combos:
-            combo = {}
-            combo['cost'] = r['cost'] + a['cost']
-            combo['armor'] = r['armor'] + a['armor']
-            combo['damage'] = r['damage'] + a['damage']
-            armor_ring_combos.append(combo)
-    
-    return sorted(armor_ring_combos, key= lambda x: x['cost'])
+        for rcombo in ring_combos:
+            total = {
+                'cost': a['cost'] + sum(r['cost'] for r in rcombo),
+                'damage': a['damage'] + sum(r['damage'] for r in rcombo),
+                'armor': a['armor'] + sum(r['armor'] for r in rcombo)
+            }
+            combos.append(total)
+    return combos
 
-cost = 0
-def add_item(player, item):
-    global cost
-    player['armor'] += item['armor']
-    player['damage'] += item['damage']
-    cost += item['cost']
 
-def remove_item(player, item):
-    global cost
-    player['armor'] -= item['armor']
-    player['damage'] -= item['damage']
-    cost -= item['cost']
+def does_player_win(player, boss):
+    player_turns = -(-boss['hp'] // max(player['damage'] - boss['armor'], 1))
+    boss_turns = -(-player['hp'] // max(boss['damage'] - player['armor'], 1))
+    return player_turns <= boss_turns
 
-def solve(input):
-    global cost
-    weapons = [x for x in input if x['type'] == 'Weapons']
-    armor_ring_combos = get_armor_ring_combos()
-    
-    player = {
-        'hp': 100,
-        'damage': 0,
-        'armor': 0,
-    }
 
-    boss = {
-        'hp': 109,
-        'damage': 8,
-        'armor': 2
-    }
+def solve_part_1(input_items):
+    weapons = [x for x in input_items if x['type'] == 'Weapons']
+    armor_ring_combos = get_armor_ring_combos(input_items)
 
-    def wins_fight(player, boss):
-        while player['hp'] > 0 and boss['hp'] > 0:
-            boss['hp'] -= max(player['damage'] - boss['armor'], 1)
-            if boss['hp'] > 0:
-                player['hp'] -= max(boss['damage'] - player['armor'], 1)
-        wins = player['hp'] > 0
-        player['hp'] = 100
-        boss['hp'] = 109
-        return wins
+    boss = {'hp': 109, 'damage': 8, 'armor': 2}
+    max_cost = float('inf')
 
-    min_cost = float('inf')
-    
-    for weapon in weapons:
-        cost = 0
-        add_item(player, weapon)
-        if wins_fight(player, boss):
-            min_cost = min(min_cost, cost)
-            remove_item(player, weapon)
-            continue
-        
-        for x in armor_ring_combos:
-            add_item(player, x)
-            if wins_fight(player, boss):
-                min_cost = min(min_cost, cost)
-                remove_item(player, x)
-                break
-            remove_item(player, x)
-        
-        remove_item(player, weapon)
-    return min_cost
-
-# solution = solve(input)
-# print(solution)
-
-# Part 2
-
-def solve_part_2(input):
-    global cost
-    weapons = [x for x in input if x['type'] == 'Weapons']
-    armor_ring_combos = get_armor_ring_combos()
-
-    player = {
-        'hp': 100,
-        'damage': 0,
-        'armor': 0,
-    }
-
-    boss = {
-        'hp': 109,
-        'damage': 8,
-        'armor': 2
-    }
-
-    def wins_fight(player, boss):
-        while player['hp'] > 0 and boss['hp'] > 0:
-            boss['hp'] -= max(player['damage'] - boss['armor'], 1)
-            if boss['hp'] > 0:
-                player['hp'] -= max(boss['damage'] - player['armor'], 1)
-        wins = player['hp'] > 0
-        player['hp'] = 100
-        boss['hp'] = 109
-        return wins
-
-    max_cost = 0
-
-    for weapon in weapons:
-        cost = 0
-        add_item(player, weapon)
-        if not wins_fight(player, boss):
-            max_cost = max(max_cost, cost)
-
-        for armor_ring_combo in armor_ring_combos:
-            add_item(player, armor_ring_combo)
-            print(cost)
-            if not wins_fight(player, boss):
-                max_cost = max(max_cost, cost)
-
-            remove_item(player, armor_ring_combo)
-
-        remove_item(player, weapon)
+    for w in weapons:
+        for ar in armor_ring_combos:
+            player = {
+                'hp': 100,
+                'damage': w['damage'] + ar['damage'],
+                'armor': w['armor'] + ar['armor']
+            }
+            total_cost = w['cost'] + ar['cost']
+            if does_player_win(player, boss):
+                max_cost = min(max_cost, total_cost)
     return max_cost
 
-part_2 = solve_part_2(input)
-print(part_2)
-# 171 - too low
+
+def solve_part_2(input_items):
+    weapons = [x for x in input_items if x['type'] == 'Weapons']
+    armor_ring_combos = get_armor_ring_combos(input_items)
+
+    boss = {'hp': 109, 'damage': 8, 'armor': 2}
+    max_cost = 0
+
+    for w in weapons:
+        for ar in armor_ring_combos:
+            player = {
+                'hp': 100,
+                'damage': w['damage'] + ar['damage'],
+                'armor': w['armor'] + ar['armor']
+            }
+            total_cost = w['cost'] + ar['cost']
+            if not does_player_win(player, boss):
+                max_cost = max(max_cost, total_cost)
+    return max_cost
+
+part_1 = solve_part_1(input_items)
+part_2 = solve_part_2(input_items)
+
+print("Part 1:", part_1)
+print("Part 2:", part_2)
