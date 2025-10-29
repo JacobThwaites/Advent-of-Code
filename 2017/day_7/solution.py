d6cac15fd780f5b4ccf176def6d1222955779b70
Jacob Thwaites <jakethwaites@gmail.com>
2025-10-29 22:36:13 +0100

    2017 day 7 part 2

--- 2017/day_7/solution.py
+++ 2017/day_7/solution.py
@@ -1,24 +1,27 @@
+# <keep your imports and get_input() and tree construction exactly as before>
 import sys
 import math
 import bisect
-from math import gcd,floor,sqrt,log
+from math import gcd, floor, sqrt, log
 from collections import defaultdict as dd
 from bisect import bisect_left as bl, bisect_right as br
 
 sys.setrecursionlimit(100000000)
 
-ceil   =lambda x: int(x) if(x==int(x)) else int(x)+1
-ceildiv=lambda x,d: x//d if(x%d==0) else x//d+1
 
-mod=1000000007
+def ceil(x): return int(x) if (x == int(x)) else int(x)+1
+def ceildiv(x, d): return x//d if (x % d == 0) else x//d+1
+
+
+mod = 1000000007
 
 
 def get_input():
     filename = './input.txt'
     # filename ='./test.txt'
-    with open(filename, 'r') as file: 
+    with open(filename, 'r') as file:
         input = []
-        for line in file: 
+        for line in file:
             line = line.replace('\n', '')
             line = line.split(' -> ')
             name, weight = line[0].split(' ')
@@ -28,15 +31,16 @@ def get_input():
                 'name': name,
                 'weight': int(weight)
             }
-            
+
             if len(line) == 2:
                 line[1] = line[1].replace(',', '')
                 leaves = line[1].split(' ')
                 info['leaves'] = leaves
-                
+
             input.append(info)
 
-        return input 
+        return input
+
 
 input = get_input()
 
@@ -45,7 +49,7 @@ tree = {}
 for row in input:
     tree[row['name']] = row
     tree[row['name']]['children'] = {}
-    
+
 nodes_to_delete = []
 for name, node in tree.items():
     if 'leaves' in node:
@@ -62,71 +66,66 @@ root = None
 for name in tree.keys():
     root = name
 # print(root)
-    
+
 # Part 2
+
 
 def find_branch_weight(node):
     weight = node['weight']
-    if len(node['children'].values()) == 0:
-        return weight
-    
     for child in node['children'].values():
         weight += find_branch_weight(child)
-        
     node['total_branch_weight'] = weight
     return weight
 
-def are_branches_balanced(node):
-    if len(node['children'].values()) == 0:
-        return True
-    
-    weights = []
-    comb = []
-    
-    for child in node['children'].values():
-        weight = find_branch_weight(child)
-        weights.append(weight)
-        comb.append((child['name'], weight))
-        
-    for i, _ in enumerate(weights[1:]):
-        if weights[i] != weights[i-1]:
-            return False 
-    
-    return True
-
-for child in tree[root]['children'].values():
-    are_branches_balanced(child)
-    
-def find_odd_one_out(name_weights):
-    totals = dd(int)
-    
-    for (name, weight) in name_weights:
-        totals[weight] += 1
-    print(totals)
-    for w, count in totals.items():
-        if count == 1:
-            for (name, weight) in name_weights:
-                if weight == w:
-                    print(name, w)
-                    return name
 
 def find_cause(node):
-    child_nodes = node['children'].values()
+    for child in node['children'].values():
+        res = find_cause(child)
+        if res is not None:
+            return res
+
+    child_nodes = list(node['children'].values())
     if len(child_nodes) == 0:
-        return 
-    
-    if len(child_nodes) < 2:
-        for node in child_nodes:
-            find_cause(node)
-        
-    if are_branches_balanced(node):
-        for node in child_nodes:
-            find_cause(node)
-    else:
-        name_weights = [(n['name'], n['total_branch_weight']) for n in node['children'].values()]
-        odd = find_odd_one_out(name_weights)
-        find_cause(node['children'][odd])
+        return None
+
+    name_weights = [(n['name'], n['total_branch_weight'], n['weight'])
+                    for n in child_nodes]
+    weights_only = [w for (_, w, _) in name_weights]
+
+    if len(set(weights_only)) == 1:
+        return None
+
+    freq = dd(int)
+    for _, w, _ in name_weights:
+        freq[w] += 1
+
+    common_weight = None
+    odd_weight = None
+    for w, cnt in freq.items():
+        if cnt == 1:
+            odd_weight = w
+        else:
+            common_weight = w
+
+    if common_weight is None:
+        common_weight = max(freq.items(), key=lambda x: x[1])[0]
+
+    odd_child = None
+    for name, tot_w, own_w in name_weights:
+        if tot_w == odd_weight:
+            odd_child = (name, tot_w, own_w)
+            break
+
+    if odd_child is None:
+        return None
+
+    diff = common_weight - odd_child[1]
+    corrected_weight = odd_child[2] + diff
+
+    return corrected_weight
 
 
-find_cause(tree[root])
-    
+find_branch_weight(tree[root])
+
+res = find_cause(tree[root])
+print(res)
